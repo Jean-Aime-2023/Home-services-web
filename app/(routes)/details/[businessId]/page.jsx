37e495b7a0dbd912c47c75c24d7c18036de27b9a
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-03-05 18:10:48 +0200

    Project page

---
+++ app/(routes)/details/[businessId]/page.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { signIn, useSession } from 'next-auth/react'
+import React from 'react'
+
+const BusinessDetail = () => {
+    const { data, status } = useSession();
+
+    if(status == 'loading')
+    {
+        return <p>Loading...</p>
+    }
+    if(status=='unauthenticated'){
+        signIn('descope');
+    }
+    return status=='authenticated' && (
+        <div>BusinessDetail</div>
+    )
+}
+
+export default BusinessDetail
--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const BusinessList = ({ businessList, title }) => {
@@ -8,7 +9,7 @@ const BusinessList = ({ businessList, title }) => {
             <h2 className='font-bold text-[22px]'>{title}</h2>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5'>
                 {businessList.length > 0 ? businessList.map((business, index) => (
-                    <div key={index} className='shadow-md rounded-lg hover:shadow-lg hover:shadow-primary cursor-pointer hover:scale-100 transition-all ease-in-out'>
+                    <Link href={'/details/'+business.id} key={index} className='shadow-md rounded-lg hover:shadow-lg hover:shadow-primary cursor-pointer hover:scale-100 transition-all ease-in-out'>
                         <Image src={business.images[0].url} alt={business.name} width={500} height={200} className='h-[150px] md:h-[200px] object-cover rounded-lg' />
                         <div className='flex flex-col items-baseline p-3 gap-2'>
                             <h2 className='p-1 bg-purple-200 text-primary rounded-full px-2 text-[12px]'>{business.category.name}</h2>
@@ -17,7 +18,7 @@ const BusinessList = ({ businessList, title }) => {
                             <h2 className='text-gray-500 text-sm'>{business.address}</h2>
                             <Button className="rounded-lg mt-2">Book Now</Button>
                         </div>
-                    </div>
+                    </Link>
                 ))
                     :
                     [1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
